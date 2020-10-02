c0502764c70a554ff1e4d2e660370b4c096b5952
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-10-02 09:57:13 +0200

    Add files via upload

---
+++ Ejercicios_01/ejerciciocopia6.php
@@ -0,0 +1,50 @@
+<?php
+$num1 = random_int(1, 10);
+
+?>
+<table border="1">
+	<tr>
+		<td>Tabla del <?php echo $num1 ?> </td>
+		<td></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 1?></td>
+		<td><?php echo $num1 * 1?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 2?></td>
+		<td><?php echo $num1 * 2?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 3?></td>
+		<td><?php echo $num1 * 3?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 4?></td>
+		<td><?php echo $num1 * 4?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 5?></td>
+		<td><?php echo $num1 * 5?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 6?></td>
+		<td><?php echo $num1 * 6?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 7?></td>
+		<td><?php echo $num1 * 7?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 8?></td>
+		<td><?php echo $num1 * 8?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 9?></td>
+		<td><?php echo $num1 * 9?></td>
+	</tr>
+	<tr>
+		<td><?php echo $num1 . " x " . 10?></td>
+		<td><?php echo $num1 * 10?></td>
+	</tr>
+</table>
