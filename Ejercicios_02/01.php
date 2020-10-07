2766fae7417014e856a135d92e413db024c7314f
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-10-07 12:00:23 +0200

    Add files via upload

--- Ejercicios_02/01.php
+++ Ejercicios_02/01.php
@@ -2,10 +2,23 @@
 $a=random_int(0, 9);
 $b=random_int(0, 9);
 $c=random_int(0, 9);
+
+echo "A= " . $a . "</br>";
+echo "B= " . $b . "</br>";
+echo "C= " . $c . "</br>";
+
 function elMayor($a, $b, $c){
     if ($a > $b){
         if ($a > $c){
-            
+            return $a;
+        }else {
+            return $c;
         }
+    }else if ($b > $c){
+        return $b;
+    }else{
+        return $c;
     }
 }
+
+echo elMayor($a, $b, $c);
