f77b9f782ef2b321f8e8c33a50bfebbfb1864ef7
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-10-26 13:33:43 +0100

    Add files via upload

--- Ejercicios_03/05.php
+++ Ejercicios_03/05.php
@@ -2,20 +2,33 @@
 $bono = array();
 $i=0;
 do{
-    $numero = random_int(1, 6);
-  
+    $numero = random_int(1, 49);
+    $repe = array_search($numero,$bono); 
     
-    $repe = array_search($numero,$bono);
-    //$repe = isset($bono[$numero]);
-    echo $repe . "</br>";
-
-    
-    
-    if($repe == false){
+    if($repe === false){
         $bono[$i]= $numero;
         $i++;
     }
 }while($i<5);
 
+asort($bono);
+$complementario = random_int(1, 49);
+
+
 echo "<pre>";
 print_r($bono);
+
+?>
+<table border="1">
+  <tr>
+    <?php 
+            foreach ($bono as $clave => $valor){
+                echo "<th>";
+                echo $valor;
+                echo "</th>";
+            }
+    ?>
+    <th>Complementario <?php echo $complementario;?></th>
+  </tr>
+</table>
+
