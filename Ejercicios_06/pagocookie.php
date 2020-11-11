1af45ade3da6471288602d5507cc9712f12c4949
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-11-11 12:32:52 +0100

    Add files via upload

---
+++ Ejercicios_06/pagocookie.php
@@ -0,0 +1,9 @@
+<?php 
+//UTILIZANDO LAS COOKIES
+//Usando pagocookie.php la selección de tarjeta 
+//se mantendrá mientras el cookie no se elimine
+//o caduque aunque rearranquemos el navegador
+
+
+
+?>
---
+++ Ejercicios_06/pagosesion.php
@@ -0,0 +1,8 @@
+<?php 
+//UTILIZANDO LAS SESIONES
+//Usando pagosesion.php la selección de tarjeta 
+//se mantendrá mientras no cerremos el navegador 
+//o mientras no pase un tiempo fijado al crear la sesión.
+
+
+?>
