1b8831b3c57f8bcfcb9f590cd9ae9cca6ddbcf9d
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-11-18 13:45:37 +0100

    Add files via upload

--- Mini_Casino/Mini_Casino.php
+++ Mini_Casino/Mini_Casino.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+$visitas=0;
 
 if (isset($_POST['dinero'])){
     $_SESSION['dinero'] = $_POST['dinero'];
@@ -9,8 +10,14 @@ if (isset($_POST['dinero'])){
 $dinero_total = $_SESSION['dinero'];
 
 
-//SOLO SE MUESTRA EN JUEGO
-if (isset($_POST['apuesta'])){
+if (isset($_POST["dejar"]) || ($_SESSION['dinero'] == '0') ) {
+    echo "Muchas gracias por jugar con nosotros. <br> ";
+    echo "Su resultado final es de " . $dinero_total . " Euros <br>";
+    $visitas++;
+    setcookie("visitascasino",$visitas, time()+ 30 * 24 * 3600); // Un mes
+    session_destroy();
+    exit();
+} else if (isset($_POST['apuesta'])){
         
     //APUESTA PAR O IMPAR ORDENADOR - ALEATORIO
     $num = random_int(0, 1);
@@ -27,34 +34,25 @@ if (isset($_POST['apuesta'])){
         echo "RESULTADO DE LA APUESTA : " . $resultado . "</br>";
         if ($_POST['num'] == "PAR" && $resultado == "PAR"){
             echo "</br> GANASTE </br>";
-            $casino=1;
+            $dinero_total = $dinero_total + $_POST['apuesta'];
         } else if ($_POST['num'] == "IMPAR" && $resultado == "IMPAR"){
             echo "</br> GANASTE </br>";
-            $casino=1;
+            $dinero_total = $dinero_total + $_POST['apuesta'];
         }else {
             echo "</br> PERDISTE... </br>";
-            $casino=0;
-        }
-        
-        //CALCULOS DE APUESTAS
-        if ($casino == 0){
             $dinero_total = $dinero_total - $_POST['apuesta'];
-        } else{
-            $dinero_total = $dinero_total + $_POST['apuesta'];
         }
-        
+       
+               
         $_SESSION['dinero'] = $dinero_total;
         
-    } else if($dinero_total == 0) {
-        echo "Error: NO PUEDES SEGUIR JUGANDO, NO TIENES DINERO. </br>";
     } else {
         echo "Error:  NO DISPONES DE " . $_POST['apuesta'] . " EUROS PARA JUGAR... </br>";
     }
-    
-    
+    header("Refresh:0");
 }
 
-echo "Dispone de " . $dinero_total . " para jugar. </br>";
+echo "Dispone de " . $_SESSION['dinero'] . " para jugar. </br>";
 ?>
 
 <html>
@@ -62,7 +60,7 @@ echo "Dispone de " . $dinero_total . " para jugar. </br>";
 	<form action="Mini_Casino.php" method="post">
 		<p>Cantidad a apostar: <input type="text" name="apuesta" /></p>
 		<p>Tipo de apuesta: <input type="radio" name="num" value="PAR" checked>Par</input>  <input type="radio" name="num" value="IMPAR">Impar</input> 
-		<p><input type="submit" value="Apostar cantidad" /> <input type="" value="Abandonar el Casino" /></p>
+		<p><input type="submit" value="Apostar cantidad" /> <button name='dejar' value='dejar'> Abandonar el Casino </button></p>
 	</form>
 </body>
 </html>
