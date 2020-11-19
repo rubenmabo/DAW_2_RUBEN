2be426fa539321230037d9307c8d7f71fa876d89
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2020-11-19 14:23:05 +0100

    Add files via upload

---
+++ Ejercicios_07/contador.php
@@ -0,0 +1,25 @@
+<?php 
+
+$absoluto = file_get_contents('accesos.txt');
+
+if (isset($_COOKIE['conexiones'])){
+    setcookie('conexiones',$_COOKIE['conexiones']+1,time()+3600*24);
+} else {  
+    setcookie('conexiones',1,time()+3600*24);
+}
+
+fwrite('accesos.txt', $_COOKIE['conexiones']);
+
+$absoluto++;
+
+file_put_contents('accesos.txt', $absoluto);
+
+/*
+$fichero_escribir = fopen("accesos.txt", "w");
+fwrite($fichero_escribir, "1");
+fclose($fichero_escribir);
+*/
+
+echo "Cookie " . $_COOKIE['conexiones'] . "</br>";
+fclose('accesos.txt');
+?>
